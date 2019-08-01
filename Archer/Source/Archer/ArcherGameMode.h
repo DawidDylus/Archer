e79ecbf0b5f85ed583a5089cc0afb4954a7b7f6d
Dawid Dylus <dylusdawid@gmail.com>
2019-08-01 14:42:05 +0200

    Setting up a project

---
+++ Archer/Source/Archer/ArcherGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcherGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AArcherGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AArcherGameMode();
+};
+
+
+
